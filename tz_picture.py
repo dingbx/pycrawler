8c5c34658ff144e2981c58de581a37d96e05aed0
ding <dingyj.no6@gmail.com>
2016-04-02 20:49:11 +0800

    add some notes

--- tz_picture.py
+++ tz_picture.py
@@ -1,7 +1,8 @@
-#coding=utf-8
-#爬取百度贴吧某个帖子里的图片，并存储到以帖子id命名的文件夹中
-#贴吧的图经过缩图，所以从http://imgsrc.baidu.com/forum/pic/item/直接获取
-#不能过滤广告和图标类图片
+# coding=utf-8
+# 爬取百度贴吧某个帖子里的图片，并存储到以帖子id命名的文件夹中
+# 贴吧的图经过缩图，所以从http://imgsrc.baidu.com/forum/pic/item/直接获取
+# author: dingbx
+# date: 2016-4-2
 
 import urllib2
 import urllib
@@ -9,20 +10,23 @@ import re
 import os
 
 
+#Description    : 抓取某一帖子图片
 class TiebaImgCrawler(object):
+    #x为图片编号，webpage为帖子网址
     def __init__(self):
         self.x = 0
         self.webpage = ""
+    #获取html页面中的图片并存到path里
     def getImg(self, html, path):
-        reg = r'<img class="BDE_Image".+src="(.+?\.jpg)"'
+        reg = r'<img class="BDE_Image".+src="(.+?\.jpg)"'    #有时候需要修改正则表达式
         imgre = re.commypile(reg)
         imglist = re.findall(imgre,html)
         for imgurl in imglist:
-            bigurl = "http://imgsrc.baidu.com/forum/pic/item/" + imgurl.split('/')[-1]
+            bigurl = "http://imgsrc.baidu.com/forum/pic/item/" + imgurl.split('/')[-1]    #获取原图
             print bigurl
             urllib.urlretrieve(bigurl, path+'/%s.jpg' % self.x)
             self.x+=1
-    
+    #主函数，循环爬取帖子里的所有页直到最后一页，将图片存在帖子id的文件夹内
     def crawl(self):
         subpage = ""
         nextpagepat = re.compile(r'<a href="(.*)">下一页</a>')
@@ -33,7 +37,6 @@ class TiebaImgCrawler(object):
         while (True):
             #webpage = "http://www.baidu.com"
             html = urllib2.urlopen(self.webpage + subpage).read()
-            #pat = re.compile('')
             m = re.findall(nextpagepat,html)
             self.getImg(html, tzid)
             if m:
